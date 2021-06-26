b0b7cdd66125a4cc0f2d76a8279f535d3aa57fe3
Deep Raj <dpraj049@gmail.com>
2021-06-26 22:10:49 +0530

    keeper App part2-Render all the notes inside notes.js as a seperate Note component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
-
-function App(){
-    return (
-        <div>
-            <Header />
-            <Note />
-            <Footer />
-        </div>
-    );
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map( (noteItem) => <Note key={noteItem.key} title={noteItem.title} content={noteItem.content} /> )}
+      {/* <Note 
+        title = {notes[0].title}
+        content = {notes[0].content}
+      />
+      <Note 
+        title = {notes[1].title}
+        content = {notes[1].content}
+      /> */}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
